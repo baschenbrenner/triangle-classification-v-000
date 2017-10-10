76b89c58bef56e662dae058703360c196abcbe8a
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-10 14:16:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
     @sides=[]
     if side1<=0 || side2<=0 || side3<=0
         raise TriangleError
-    elsif side1+side2<side3 || side2+side3<side1 || side1+side3<side2
+    elsif side1+side2<=side3 || side2+side3<=side1 || side1+side3<=side2
         raise TriangleError
     else
       @sides << side1
