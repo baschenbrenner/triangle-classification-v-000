0232beb1a651fe28ad68ffab2aa564a9a1f6b18d
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-10 13:51:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+
+  def initialize(side1,side2,side3)
+    @side1=side1
+    @side2=side2
+    @side3=side3
+    
+  end
+
+  def kind
+
+    side_array=[]
+    side_array <<side1
+    side_array <<side2
+    side_array <<side3
+    
+    if side_array.uniq.length == 1
+      return :equilateral
+    elsif side_array.uniq.length == 2
+      return :isosceles
+    elsif  side_array.uniq.length == 3
+      return scalene
+    end
+  end
+  
+  
 end
+
+
+class TriangleError < StandardError
+  
