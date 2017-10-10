a62cc7377e1e251706608a6d016a68bd7d50f0f6
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-10 14:14:17 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,28 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :sides
 
 
   def initialize(side1,side2,side3)
+    @sides=[]
     if side1<=0 || side2<=0 || side3<=0
         raise TriangleError
+    elsif 
+        
     else
-
-      @side1=side1
-      @side2=side2
-      @side3=side3
+      @sides << side1
+      @sides << side2
+      @sides << side3
     end
 
 
   end
 
   def kind
-
-    side_array=[]
-    side_array <<side1
-    side_array <<side2
-    side_array <<side3
-
-    if side_array.uniq.length == 1
+    if self.sides.uniq.length == 1
       return :equilateral
-    elsif side_array.uniq.length == 2
+    elsif self.sides.uniq.length == 2
       return :isosceles
-    elsif  side_array.uniq.length == 3
+    elsif  self.sides.uniq.length == 3
       return :scalene
     end
   end
